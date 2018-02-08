c2b0651ccc1de52f94e2921d9ec35e73e63e1e27
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2018-02-07 22:15:06 -0600

    Initial commit

---
+++ src/pkg15460644p01/Main.java
@@ -0,0 +1,18 @@
+/*
+* Este programa fue hecho por Mario Josue del Toro Morales
+* Estudiante del sexto semestre de la carrera de Ingeniería en Sistemas Computacionales
+* del Instituto Tecnológico de Colima
+* Materia de Lenguajes y Autómatas I
+******Descripción************
+*Programa que obtiene la inversa y potencia de una cadena dada.
+*/
+package pkg15460644p01;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Principal principal = new Principal(); // Declaración de la clase que tiene el formato gráfico
+        principal.setVisible(true); //Despliegue de la ventana gráfica
+    }
+    
+}
